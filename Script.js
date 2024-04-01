1d2c9f2c64c2dedea07a6662a0e3b4268e2a1c05
Sabarish <sabarishharidas@gmail.com>
2024-04-01 11:32:48 +0530

    Db Created

--- Script.js
+++ Script.js
@@ -1,1 +1,25 @@
-console.log("helloo");
+const mongoose = require("mongoose");
+const User = require("./User");
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+    const user = await User.create({
+      name: "sabarish",
+      age: 22,
+      email: "sabarishharidas@gmail.com",
+      Skills: ["FrontEnd", "BackEnd"],
+      address: {
+        Street: "Iraniyan Street",
+        City: "Erode",
+      },
+    });
+
+    console.log(user);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+main();
--- User.js
+++ User.js
@@ -1,1 +1,40 @@
-const mongoose = require('mon')
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  Street: String,
+  City: String,
+});
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: {
+    type: Number,
+    min: 1,
+    max: 100,
+    validate: {
+      validator: (v) => v % 2 === 0,
+      message: (props) => `${props.value} is not even number`,
+    },
+  },
+  email: {
+    type: String,
+    minLength: 10,
+    required: true,
+    lowercase: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    immutable: true,
+    default: Date.now(),
+  },
+  Freind: mongoose.SchemaTypes.ObjectId,
+  Skills: [String],
+  address: addressSchema,
+});
+
+module.exports = mongoose.model("User", UserSchema);
